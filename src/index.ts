c40854d56e4f4d45f8aa2c94f2cae95e897472fe
Botz <127378435+BotzC@users.noreply.github.com>
2026-02-18 09:16:36 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { R2Explorer } from 'r2-explorer';
 export default R2Explorer({ basicAuth: [{
     username: 'phadmin',
     password: 'phadmin',
-    readonly: false
+    readonly: 'false'
   },{
     username: 'user',
     password: '123',
-    readonly: true
+    readonly: 'true'
   }]
 });
